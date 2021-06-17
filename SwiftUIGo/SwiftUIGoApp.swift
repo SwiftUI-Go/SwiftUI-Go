5a106f01532b7d09801093a6ec6750ecc0661ac4
lvyuan <lvyuan@bixin.cn>
2021-06-17 16:23:18 +0800

    Initial Commit

---
+++ SwiftUIGo/SwiftUIGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGoApp.swift
+//  SwiftUIGo
+//
+//  Created by lvyuan on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
