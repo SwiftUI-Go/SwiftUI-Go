b035c67373bc057142505bf22ffeb722caeaff09
lvyuan <lvyuan@bixin.cn>
2021-06-23 18:49:47 +0800

    feat: add version

--- Settings/AboutUsView.swift
+++ Settings/AboutUsView.swift
@@ -9,21 +9,33 @@ import SwiftUI
 
 struct AboutUsView: View {
     
+    @State var version = ""
+    
     private func rowCell(_ l: LocalizedStringKey, _ r: String) -> some View {
         return HStack {
             Text(l)
             Spacer()
             Text(r)
+                .foregroundColor(.gray)
         }
+    }
+    
+    private func loadInfo() {
+        guard let info = Bundle.main.infoDictionary else {
+            return
+        }
+        version = "\(info["CFBundleShortVersionString"] as! String)(\(info["CFBundleVersion"] as! String))"
     }
     
     var body: some View {
         List {
-            rowCell(LocalizeKey.version, "")
+            rowCell(LocalizeKey.version, version)
         }
         .navigationBarTitle(Text(LocalizeKey.aboutUs))
         .listStyle(InsetGroupedListStyle())
+        .onAppear(perform: loadInfo)
     }
+    
 }
 
 struct AboutUsView_Previews: PreviewProvider {
